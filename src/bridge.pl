47140f125d667857309a0072b0d50a4b2fe39c05
Juho Snellman <jsnell@iki.fi>
2013-01-12 23:38:54 +0100

    Minor noise

--- src/bridge.pl
+++ src/bridge.pl
@@ -6,10 +6,13 @@ use File::Basename qw(dirname);
 chdir dirname $0;
 
 print "Content-type: text/javascript\r\n";
-print "Cache-Control: no-cache:\r\n";
+print "Cache-Control: no-cache\r\n";
 print "\r\n";
 
-exec "perl", "../src/tracker.pl", "../test/testgame2.txt";
+my $id = $ENV{QUERY_STRING};
+$id =~ s/[^0-9]//g;
+
+exec "/usr/bin/perl", "../../git/src/tracker.pl", "../../git/test/testgame$id.txt";
 
 
 
