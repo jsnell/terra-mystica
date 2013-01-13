a80b773dc80ddd0cd70f77965b8a83fbf4d4e212
Juho Snellman <jsnell@iki.fi>
2013-01-14 00:50:01 +0100

    Actually write the output file

--- src/save.pl
+++ src/save.pl
@@ -5,6 +5,7 @@ use strict;
 use JSON;
 use POSIX qw(chdir);
 use File::Basename qw(dirname);
+use File::Temp qw(tempfile);
 use CGI qw(:cgi);
 use Fatal qw(chdir open);
 use tracker;
@@ -19,9 +20,15 @@ $id =~ s{[^A-Za-z0-9]}{}g;
 
 my $new_content = $q->param('content');
 
+my $dir = "../data/write/";
+
 sub save {
-    open my $fh, ">", "../../data/write/$id.new";
+    my ($fh, $filename) = tempfile("tmpfileXXXXXXX",
+                                   DIR=>$dir);
     print $fh $new_content;
+    close $fh;
+    rename "$dir/$id", "$dir/$id.bak";
+    rename $filename, "$dir/$id";
 }
 
 my $res = terra_mystica::evaluate_game split /\n/, $new_content;
