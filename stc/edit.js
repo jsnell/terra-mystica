c7b9e0f558758d8aab2ebed334a495075a05f9cc
Juho Snellman <jsnell@iki.fi>
2013-01-19 02:45:28 +0100

    Fallback to a plain textarea with IE 8

--- stc/edit.js
+++ stc/edit.js
@@ -1,3 +1,63 @@
+var fallback = false;
+var id = document.location.pathname;
+var editor = null;
+
+// From JQuery
+var Browser = Class.create({
+  initialize: function() {
+    var userAgent = navigator.userAgent.toLowerCase();
+    this.version = (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [])[1];
+    this.webkit = /webkit/.test( userAgent );
+    this.opera = /opera/.test( userAgent );
+    this.msie = /msie/.test( userAgent ) && !/opera/.test( userAgent );
+    this.mozilla = /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test( userAgent );
+  }
+});
+
+var browser = new Browser();
+
+function init() {
+    if (browser.msie && browser.version < 9) {
+        fallback = true;
+    }
+
+    if (fallback) {
+        $("wrapper").style.display = "none";
+        $("fallback-editor").style.display = "block";
+    } else {
+        editor = ace.edit("editor");
+        var hash;
+        editor.getSession().setMode("ace/mode/sh");
+        editor.commands.removeCommand("replace");
+        editor.commands.removeCommand("gotoline");
+
+        editor.commands.addCommand({
+            name: 'mySave',
+            bindKey: { win: 'Ctrl-S',  mac: 'Command-S' },
+            exec: save
+        });
+    }
+
+    load();
+}
+
+function getEditorContent() {
+    if (editor) {
+        return editor.getValue();
+    } else {
+        return $("fallback-editor").value;
+    }
+}
+
+function setEditorContent(data) {
+    if (editor) {
+        editor.setValue(data);
+        editor.clearSelection();
+    } else {
+        $("fallback-editor").value = data;
+    }
+}
+
 function save() {
     if ($("save-button").disabled) {
         return;
@@ -9,7 +69,7 @@ function save() {
         method: "post",
         parameters: {
             "game": id,
-            "content": editor.getValue(),
+            "content": getEditorContent(),
             "orig-hash": hash,
         },
         onFailure: function(transport) {
@@ -47,11 +107,10 @@ function load() {
         onSuccess: function(transport){
             try {
                 var res = transport.responseText.evalJSON();
-                editor.setValue(res.data);
-                editor.clearSelection();
+                setEditorContent(res.data);
                 hash = res.hash;
 
-                var title = editor.getValue().match("# (.*)");
+                var title = getEditorContent().match("# (.*)");
                 if (title && title[1]) {
                     $("title").innerHTML += " (" + title[1] + ")";
                     $("header").innerHTML += " (" + title[1] + ")";
