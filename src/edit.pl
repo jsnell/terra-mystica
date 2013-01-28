6ebc9b7ca0b11869ed4d76c940fc32e051ca1a3d
Juho Snellman <jsnell@iki.fi>
2013-01-28 23:28:18 +0100

    Revert "Move most cgi logic to modules"
    This reverts commit 6d4e8c8dfff093e75df61dd61638d3ce5d1d9203.

--- src/edit.pl
+++ src/edit.pl
@@ -1,10 +1,35 @@
 #!/usr/bin/perl -w
 
+use CGI qw(:cgi);
+use Digest::SHA1  qw(sha1_hex);
+use Fatal qw(chdir open);
 use File::Basename qw(dirname);
+use File::Slurp;
+use JSON;
+
 chdir dirname $0;
 
-BEGIN { push @INC, "." }
+my $q = CGI->new;
 
-use edit;
+my $id = $q->param('game');
+$id =~ s{.*/}{};
+$id =~ s{[^A-Za-z0-9_]}{}g;
 
-serve "../data/";
+my $file = "../../data/write/$id";
+
+if (!-f $file) {
+    die "Can't open $file";
+}
+
+my $data = read_file($file);
+
+my $out = encode_json {
+    data => $data,
+    hash => sha1_hex($data),
+};
+
+print "Content-type: text/json\r\n";
+print "Cache-Control: no-cache\r\n";
+print "\r\n";
+
+print $out;
