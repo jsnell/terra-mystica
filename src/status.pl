04e4a602751d55e10d5d96b4ce5895e0189f9d1d
Juho Snellman <jsnell@iki.fi>
2013-02-16 20:39:28 +0100

    Print to stdout

--- src/status.pl
+++ src/status.pl
@@ -11,12 +11,12 @@ BEGIN { push @INC, dirname $0; }
 use tracker;
 
 sub print_status {
-    printf(STDERR "%25s % 5s % 5s % 5s % 8s % 5s\n",
+    printf("%25s % 5s % 5s % 5s % 8s % 5s\n",
            qw(Faction W C P PW VP));
-    print STDERR '-' x 65;
+    print '-' x 65;
     for my $faction_name (@factions) {
         my $faction = $factions{$faction_name};
-        printf(STDERR "%25s % 5d % 5d % 5d % 8s % 5d\n",
+        printf("%25s % 5d % 5d % 5d % 8s % 5d\n",
                $faction->{display},
                $faction->{W}, 
                $faction->{C}, 
@@ -24,10 +24,10 @@ sub print_status {
                (sprintf "%d/%d/%d", $faction->{P1}, $faction->{P2}, $faction->{P3}),
                $faction->{VP});
     }
-    print STDERR '-' x 65;
+    print '-' x 65;
 }
 
-my $res = evaluate_game <>;
+my $res = evaluate_game { rows => [ <> ] };
 print_status $res;
 
 if ($res->{error}) {
