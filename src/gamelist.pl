998cfbd2c365176b09ddfd59da93cdb54fa083e1
Juho Snellman <jsnell@iki.fi>
2013-02-24 09:54:09 +0100

    More sensible sorting for games in game list

--- src/gamelist.pl
+++ src/gamelist.pl
@@ -4,17 +4,42 @@ use POSIX qw(chdir);
 use File::Basename qw(dirname);
 use JSON;
 
-chdir dirname $0;
+sub split_nums {
+    local $_ = shift;
+    /(\d+|\D+)/g;
+}
+
+sub natural_cmp {
+    my @a = split_nums shift;
+    my @b = split_nums shift;
+
+    while (@a and @b) {
+        my $a = shift @a;
+        my $b = shift @b;
+        next if $a eq $b;
+
+        if ($a =~ /\d/ and $b =~ /\d/) {
+            return $a <=> $b;
+        } else {
+            return $a cmp $b;
+        }
+    }
+
+    return @a <=> @b;
+}
 
 print "Content-type: text/javascript\r\n";
 print "Cache-Control: no-cache\r\n";
 print "\r\n";
 
+chdir dirname $0;
 my @ids = glob "../../data/read/*";
 s{.*/}{} for @ids;
 
 print encode_json [ map {
                       { id => $_ }   
+                    } sort {
+                        natural_cmp $a, $b;
                     } @ids ];
 
 
