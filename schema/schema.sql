42726d2368ec8b59f5073a394e6b947b3f57a96c
Juho Snellman <jsnell@iki.fi>
2013-04-14 00:54:24 +0200

    Initial metadata schema

---
+++ schema/schema.sql
@@ -0,0 +1,26 @@
+create table player (
+  username text primary key,
+  password text
+);
+
+create table email (
+  address text unique,
+  player text references player (username),
+  validated boolean
+);
+
+create table game (
+    id text primary key,
+    write_id text,
+    needs_indexing boolean
+);
+
+create table game_role (
+    game text references game (id),
+    email text, -- Conceptually references email (address), but not enforced
+    faction text,
+    primary key (game, faction)
+);
+
+
+
