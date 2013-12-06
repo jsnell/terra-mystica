f59002456950486d707c11b497e1c9e84f018fd3
Juho Snellman <jsnell@iki.fi>
2013-12-06 18:08:32 +0100

    fixup gamelist

--- src/gamelist.pl
+++ src/gamelist.pl
@@ -66,7 +66,7 @@ if ($mode eq 'all') {
         $res{error} = "Not logged in <a href='/login/'>(login)</a>"
     } else {
         my @roles = $dbh->selectall_arrayref(
-            "select game, faction, game.write_id, game.finished, action_required, (extract(epoch from now() - game.last_update)) as time_since_update, vp, rank, (select faction from game_role as gr2 where gr2.game = gr1.game and action_required limit 1) as waiting_for, leech_required, game.round, (select count(*) from chat_message where game=game.id and posted_at > (select coalesce((select last_read from chat_read where game=chat_message.game and player=?), '2012-01-01'))) as unread_chat from game_role as gr1 left join game on game=game.id where email in (select address from email where player = ? and (game.finished = ? or last_update > now() - interval '2 days') and (gr1.faction = 'admin') = ?)",
+            "select game, faction, game.write_id, game.finished, action_required, (extract(epoch from now() - game.last_update)) as time_since_update, vp, rank, (select faction from game_role as gr2 where gr2.game = gr1.game and action_required limit 1) as waiting_for, leech_required, game.round, (select count(*) from chat_message where game=game.id and posted_at > (select coalesce((select last_read from chat_read where game=chat_message.game and player=?), '2012-01-01'))) as unread_chat from game_role as gr1 left join game on game=game.id where email in (select address from email where player = ? and (game.finished = ? or (game.finished and last_update > now() - interval '2 days')) and (gr1.faction = 'admin') = ?)",
             {}, $user, $user, $status{$status}, 1*!!($mode eq 'admin'));
         add_sorted map {
             { id => $_->[0],
