ba77aeeeb375059a6b502e969717f2dd164f03bc
Juho Snellman <jsnell@iki.fi>
2013-12-08 22:28:08 +0100

    Fix diffgame

--- src/tracker.pl
+++ src/tracker.pl
@@ -25,11 +25,16 @@ sub print_json {
 
 my $dbh = get_db_connection;
 
-my @rows = get_game_commands $dbh, $ARGV[0];
+my $id = $ARGV[0];
+my @rows = get_game_commands $dbh, $id;
 
 # @rows = @rows[0..(min $ENV{MAX_ROW}, scalar(@rows)-1)];
 
-my $res = evaluate_game { rows => [ @rows ] };
+my $res = evaluate_game {
+    rows => [ @rows ],
+    faction_info => get_game_factions($dbh, $id),
+    players => get_game_players($dbh, $id),
+};
 print_json $res;
 
 if (scalar @{$res->{error}}) {
