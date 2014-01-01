488824e1dec6d0f2403eaaaf27bd752077eb29aa
Juho Snellman <jsnell@iki.fi>
2014-01-01 15:49:23 +0100

    Use a hash slice result for the main game listing query, for readability

--- src/DB/Game.pm
+++ src/DB/Game.pm
@@ -191,34 +191,35 @@ sub sorted_user_games {
 }
 
 sub role_link {
-    my ($dbh, $game, $role, $write_id) = @_;
-    if ($role eq 'admin') {
-        "/edit/$write_id";
+    my ($dbh, $record) = @_;
+    if ($record->{faction} eq 'admin') {
+        "/edit/$record->{write_id}";
     } else {
-        edit_link_for_faction $dbh, $write_id, $role;
+        edit_link_for_faction $dbh, $record->{write_id}, $record->{faction};
     }
 }
 
 sub get_user_game_list {
     my ($dbh, $user, $mode, $status, $admin) = @_;
 
-    my @roles = $dbh->selectall_arrayref(
+    my $roles = $dbh->selectall_arrayref(
         "select game, faction, game.write_id, game.finished, action_required, (extract(epoch from now() - game.last_update)) as time_since_update, vp, rank, (select faction from game_role as gr2 where gr2.game = gr1.game and action_required limit 1) as waiting_for, leech_required, game.round, (select count(*) from chat_message where game=game.id and posted_at > (select coalesce((select last_read from chat_read where game=chat_message.game and player=?), '2012-01-01'))) as unread_chat, game.aborted from game_role as gr1 left join game on game=game.id where email in (select address from email where player = ?) and (game.finished = ? or (game.finished and last_update > now() - interval '2 days')) and (gr1.faction = 'admin') = ?",
-        {}, $user, $user, $status, $admin);
+        { Slice => {} },
+        $user, $user, $status, $admin);
     sorted_user_games (map {
-        { id => $_->[0],
-          role => $_->[1],
-          link => ($mode eq 'other-user' ? "/game/$_->[0]" : role_link($dbh, @{$_})),
-          finished => $_->[3] ? 1 : 0,
-          action_required => !$_->[12] && ($_->[4] || $_->[9]) || 0,
-          seconds_since_update => $_->[5],
-          vp => $_->[6],
-          rank => $_->[7],
-          waiting_for => $_->[8],
-          round => $_->[10],
-          unread_chat_messages => 1*$_->[11],
-          aborted => $_->[12],
-        } } @{$roles[0]});
+        { id => $_->{game},
+          role => $_->{faction},
+          link => ($mode eq 'other-user' ? "/game/$_->{game}" : role_link($dbh, $_)),
+          finished => $_->{finished} ? 1 : 0,
+          action_required => !$_->{aborted} && ($_->{action_required} || $_->{leech_required}) || 0,
+          seconds_since_update => $_->{time_since_update},
+          vp => $_->{vp},
+          rank => $_->{rank},
+          waiting_for => $_->{waiting_for},
+          round => $_->{round},
+          unread_chat_messages => 1*$_->{unread_chat},
+          aborted => $_->{aborted},
+        } } @{$roles});
 }
 
 1;
