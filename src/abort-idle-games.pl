db4d7a33cebc9b9d422c49d99b9561036d3a2a6a
Juho Snellman <jsnell@iki.fi>
2014-01-29 01:17:55 +0100

    Abort idle games after 2 weeks

---
+++ src/abort-idle-games.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use File::Basename;
+
+BEGIN { push @INC, dirname $0 }
+
+use DB::Connection;
+
+my $dbh = get_db_connection;
+
+$dbh->do("begin");
+my $count = $dbh->do("update game set aborted=true, finished=true where last_update < now() - interval '2 weeks' and not finished",
+                     {},
+                     ());
+print STDERR "Aborting $count games\n";
+$dbh->do("commit");
