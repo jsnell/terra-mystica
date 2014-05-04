923e77ea283c1ef96a89d8cd76e8dcdf204fa89c
Juho Snellman <jsnell@iki.fi>
2014-05-04 18:40:03 +0200

    Utility for removing a game

---
+++ src/drop-game.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -lw
+
+use strict;
+no indirect;
+
+use DBI;
+
+BEGIN { push @INC, "$ENV{PWD}/src/"; }
+
+use DB::Connection;
+
+my $dbh = get_db_connection;
+
+$dbh->do("begin");
+
+my $id = shift;
+die "No game id supplied" if !defined  $id;
+
+print $dbh->do("delete from game_role where game=?", {}, $id);
+print $dbh->do("delete from game_player where game=?", {}, $id);
+print $dbh->do("delete from game where id=?", {}, $id);
+
+$dbh->do("commit");
