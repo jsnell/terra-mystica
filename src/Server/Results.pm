59ae9095f56e1ad02746fa4d8d0f8cf831206f83
Juho Snellman <jsnell@iki.fi>
2014-08-21 05:27:30 +0200

    Add a result feed format with less duplication

--- src/Server/Results.pm
+++ src/Server/Results.pm
@@ -12,23 +12,62 @@ use DB::Connection;
 use DB::Game;
 use DB::Secret;
 
-method handle($q, $month) {
+method handle($q, $params) {
     my $dbh = get_db_connection;
     my ($secret, $iv) = get_secret $dbh;
     my %params = ();
+    my $version = 'v1';
+    my @valid_versions = qw(v1 v2);
 
-    if ($month =~ m{^(\d+)/(\d+)(?:/(\d+))?$}) {
-        %params = (year => $1, month => $2, day => $3);
-    } else {
-        die "Year/month not specified (e.g. /app/results/2014/01). Day is optional (e.g. /app/results/2014/01/01)\n"
+    if ($params =~ s{^v(\d+)/}{}) {
+        $version = $1;
+        my %valid_versions = map { ($_ => 1) } @valid_versions;
+
+        if (!$valid_versions{"v$version"}) {
+            die "Invalid version 'v$version'. (Valid values: @valid_versions)\n";
+        }
     }
 
-    my $results = { get_finished_game_results $dbh, $secret, %params };
+    if ($params =~ m{^(\d+)/(\d+)(?:/(\d+))?$}) {
+        %params = (year => $1, month => $2, day => $3);
+    } else {
+        die "Year/month not specified (e.g. /app/results/$version/2014/01). Day is optional (e.g. /app/results/$version/2014/01/01)\n"
+    }
 
+    my $results = {
+        version => $version,
+        get_finished_game_results $dbh, $secret, %params
+    };
     for (@{$results->{results}}) {
         delete $_->{base_map};
     }
-    
+
+    if ($version > 1) {
+        $results->{games} = {};
+        $results->{players} = {};
+
+        for (@{$results->{results}}) {
+            my $game = ($results->{games}{$_->{game}} //= {
+                non_standard => $_->{non_standard},
+                player_count => $_->{player_count},
+                last_update => $_->{last_update},
+            });
+            delete $_->{non_standard};
+            delete $_->{player_count};   
+            delete $_->{last_update};
+
+            $results->{players}{$_->{id_hash}} //= {
+                username => $_->{username}
+            };
+            delete $_->{username};
+            delete $_->{game};
+
+            push @{$game->{players}}, $_;
+        }
+
+        delete $results->{results};
+    }
+
     $self->output_json($results);
 }
 
