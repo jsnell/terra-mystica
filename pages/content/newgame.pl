549877c15b1bbb876818ef3bd840d025d15c38c5
Juho Snellman <jsnell@iki.fi>
2014-09-05 21:46:02 +0200

    Change the layout of the "new game" page, clean up the help texts

--- pages/content/newgame.pl
+++ pages/content/newgame.pl
@@ -10,9 +10,10 @@
 __DATA__
 <div id="error"></div>
 <form id="newgame" action="/app/new-game/" onsubmit="return false">
-  <table>
+  <table class="newgame-settings">
     <tr style="vertical-align: top">
       <td style="width: 12ex">Game id<td><input name="gameid" id="gameid" oninput="javascript:newGameValidate()">
+      <td>
         <p>The id of the game should be unique, and only contain
 the letters A-Z and a-z and the digits 0-9.
     </tr>
@@ -24,6 +25,7 @@ the letters A-Z and a-z and the digits 0-9.
         <input name="game-options" type="checkbox" id="option-shipping-bonus" value="shipping-bonus"></input><label for="option-shipping-bonus">Shipping bonus tile (Spielbox 6/2013)</label><br>
         <input name="game-options" type="checkbox" id="option-email-notify" value="email-notify" onchange="javascript:newGameValidate()" checked></input><label id="option-email-notify-label" for="option-email-notify">Automatic email notifications</label><br>
         <input name="game-options" type="checkbox" id="option-maintain-player-order" value="maintain-player-order" onchange="javascript:newGameValidate()"></input><label id="option-maintain-player-order-label" for="option-maintain-player-order">Don't randomize player order (private games only)</label><br>
+      <td>
         <p>
 The options are described in more detail in the
 <a href="/usage.html#options">manual.</a>
@@ -37,6 +39,7 @@ The options are described in more detail in the
 <option value="private">Private</option>
 <option value="public">Public</option>
         </select>
+      <td>
         <p>
 Private games are created with a predetermined set of players.
 Public games can be joined by any player on the site.
@@ -44,15 +47,16 @@ Public games can be joined by any player on the site.
 
     <tr style="vertical-align: top" id="players-row" style="display: none">
       <td>Players<td><textarea name="players" id="players" style="width: 40ex; height: 6em;" oninput="javascript:newGameValidate()" placeholder="usernames or email addresses"></textarea>
+      <td>
         <p>
 You can specify the players either using a
 username or an email address (must be registered
 on the site). One player per row, at least two
-players required. <b>You can't change who is playing
-after creating the game.</b>
+players required. You can't change who is playing
+after creating the game.
         <p>
-Please do not add players to games if they aren't
-expecting it.
+<b>Please do not add players to games if they aren't
+expecting it.</b>
     </tr>
 
     <tr style="vertical-align: top" id="player-count-row" style="display: none">
@@ -64,6 +68,7 @@ expecting it.
 <option>4</option>
 <option>5</option>
 </select>
+      <td>
         <p>
 The game will start automatically once this many
 players have joined.
@@ -74,15 +79,13 @@ players have joined.
       <td>
         Minimum: <input name=min-rating id=min-rating type=text style="width: 5ex"></input>
         Maximum: <input name=max-rating id=max-rating type=text style="width: 5ex"></input>
+      <td>
         <p>
           Only players in this rating range can join the game. Leave
           one or both fields empty if you don't want any restrictions.
-          Players who are still unranked will be considered to have a
+          Players who are still unranked are considered to have a
           rating of 0; they can join games with a maximum rating but
           not games with a minimum rating.
-        <p>
-          You can't start a game if you'd be outside the rating range
-          yourself.
     </tr>
 
     </tr>
@@ -97,7 +100,7 @@ players have joined.
 <option selected=true value="168">1 week</option>
 <option value="336">2 weeks</option>
 </select>
-        
+      <td>        
         <p>
           Players will be kicked out of the game after this period of
           inactivity. The game admin can reinstate a kicked out player, but
@@ -106,6 +109,7 @@ players have joined.
 
     <tr style="vertical-align: top" id="description-row" style="display: none">
       <td>Description<td><textarea name="description" id="description" style="width: 40ex; height: 6em;" style="display: none"></textarea>
+      <td>
         <p>
 Any special information related to this game, that
 will be shown in the list of open games. Use this
