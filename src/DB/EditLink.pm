34cd4d8252d3f49b8d8e42631dbd659a83fcc51c
Juho Snellman <jsnell@iki.fi>
2015-05-19 02:18:01 +0200

    Work around for edit links breaking after Perl upgrade

--- src/DB/EditLink.pm
+++ src/DB/EditLink.pm
@@ -5,6 +5,7 @@ use Exporter::Easy (EXPORT => [ 'edit_link_for_faction' ]);
 
 use Crypt::CBC;
 use DB::Secret;
+use Encode;
 
 sub edit_link_for_faction {
     my ($dbh, $id, $faction_name) = @_;
@@ -18,9 +19,9 @@ sub edit_link_for_faction {
                                  -iv => $iv,
                                  -add_header => 0,
                                  -cipher => 'Blowfish');
-    my $data = $game_secret ^ $faction_name;
-    my $token = unpack "h*", $cipher->encrypt($data);
-
+    my $data = "$game_secret" ^ encode('iso-8859-1', "$faction_name");
+    my $token = unpack 'h*', $cipher->encrypt($data);
+    
     return "/faction/$game/".($faction_name)."/$token";
 }
 
