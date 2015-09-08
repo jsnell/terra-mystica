3948a6206b63c8cc0d3138ca89c8dae7cdde851a
Brian Graham <bcgraham+github@gmail.com>
2015-09-08 13:46:23 -0400

    Add another player info guess when finalizing.

--- src/tracker.pm
+++ src/tracker.pm
@@ -52,6 +52,7 @@ sub finalize {
         }
     }
     $game{acting}->clear_empty_actions();
+    my %faction_info_usernames = map { $faction_info->{$_}->{username} => $_ } keys $faction_info;
 
     for my $faction ($game{acting}->factions_in_order()) {
         if ($faction->{waiting}) {
@@ -68,9 +69,12 @@ sub finalize {
                 });
         }
         my $faction_count = $game{acting}->faction_count();
+        my $player_index = %faction_info_usernames{$faction->{username}};
         my $info;
         if (exists $faction_info->{$faction->{name}}) {
             $info = $faction_info->{$faction->{name}};
+        } elsif (exists $faction_info->{$player_index}) {
+            $info = $faction_info->{$player_index};
         } elsif (exists $faction_info->{"player${faction_count}"}) {
             $info = $faction_info->{"player${faction_count}"}
         }
