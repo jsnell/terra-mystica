7282cfbbcc1bebd2e6f95bf09f489ced4561d7f6
Juho Snellman <jsnell@iki.fi>
2015-12-01 01:31:52 +0100

    ui: Season 10 started

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,7 +8,7 @@
 
 __DATA__
 
-<div class="motd" style="display: block">
+<div class="motd" style="display: none">
     Season 10 of the Terra Mystica tournament will start on December 1st.
     Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1474705">BGG thread</a>.
 </div>
