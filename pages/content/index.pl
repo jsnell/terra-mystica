addb1ead8d710fe9d45d70e300acb15f03f7fe03
Juho Snellman <jsnell@iki.fi>
2016-02-02 19:02:06 +0100

    Season 11 started

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,7 +8,7 @@
 
 __DATA__
 
-<div class="motd" style="display: block">
+<div class="motd" style="display: none">
     Season 11 of the Terra Mystica tournament will start on February 1st.
     Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1510370">BGG thread</a>.
 </div>
