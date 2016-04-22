00009c4508671018083dab60ed3f6a19b692fd7c
Juho Snellman <jsnell@iki.fi>
2016-04-22 10:08:00 +0200

    Add support for batch-adding games with chess clock

--- src/add-games-batch.pl
+++ src/add-games-batch.pl
@@ -42,6 +42,13 @@ sub validate {
         }
         check_username_is_registered $dbh, $player;
     }
+    
+    my $chess_clock_settings = scalar grep { defined $_ } map { $desc->{$_} } qw(chess-clock-hours-initial chess-clock-hours-per-round chess-clock-grace-period);
+    if (($chess_clock_settings != 0 and $chess_clock_settings != 3) or
+        ($chess_clock_settings == 0 and !defined $desc->{'deadline-hours'}) or
+        ($chess_clock_settings == 3 and defined $desc->{'deadline-hours'})) {
+        die "Inconsistent chess clock / deadline settings\n";
+    }
 
     check_username_is_registered $dbh, $desc->{admin};
 
@@ -103,13 +110,24 @@ sub make_games {
                     $map_variant,
                     @{$options});
 
-        $dbh->do("insert into game_options (game, description, minimum_rating, maximum_rating, deadline_hours) values (?, ?, ?, ?, ?)",
+        
+
+        $dbh->do("update game set description=?, current_chess_clock_hours=? where id=?",
+                 {},
+                 $game_desc->{'description'},
+                 $desc->{'chess-clock-hours-initial'},
+                 $id);
+
+        $dbh->do("insert into game_options (game, description, minimum_rating, maximum_rating, deadline_hours, chess_clock_hours_initial, chess_clock_hours_per_round, chess_clock_grace_period) values (?, ?, ?, ?, ?, ?, ?, ?)",
                  {},
                  $id,
                  $game_desc->{'description'},
                  $desc->{'minimum-rating'},
                  $desc->{'maximum-rating'},
-                 $desc->{'deadline-hours'} // 120);
+                 $desc->{'deadline-hours'},
+                 $desc->{'chess-clock-hours-initial'},
+                 $desc->{'chess-clock-hours-per-round'},
+                 $desc->{'chess-clock-grace-period'});
 
         notify_game_started $dbh, {
             name => $id,
