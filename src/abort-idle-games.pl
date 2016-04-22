f28e05ae17ef2f2717052f684558ae6469646ed2
Juho Snellman <jsnell@iki.fi>
2016-04-22 10:07:36 +0200

    Update the idle game timeout to 4 weeks (games with chess clock could have 2 week idle period)

--- src/abort-idle-games.pl
+++ src/abort-idle-games.pl
@@ -11,7 +11,7 @@ use DB::Connection;
 my $dbh = get_db_connection;
 
 $dbh->do("begin");
-my $count = $dbh->do("update game set aborted=true, finished=true where last_update < now() - interval '2 weeks' and not finished",
+my $count = $dbh->do("update game set aborted=true, finished=true where last_update < now() - interval '4 weeks' and not finished",
                      {},
                      ());
 if ($count > 0) {
