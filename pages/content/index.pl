718069f92a1abecd9e4b55d100f69eea0a4b9ca1
Juho Snellman <jsnell@iki.fi>
2016-06-01 01:16:00 +0200

    season 13 starts

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,7 +8,7 @@
 
 __DATA__
 
-<div class="motd" style="display: block">
+<div class="motd" style="display: none">
     Season 13 of the Terra Mystica tournament will start on June 1st.
     Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1578126/signup-season-13-now-open">BGG thread</a>.
 </div>
