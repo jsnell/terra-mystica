148603f00ab941f4b1d88ac93d4ddf7f00192f7a
Juho Snellman <jsnell@iki.fi>
2016-06-01 09:01:14 +0200

    Do a hard exit rather than an exception on watchdog timeout
    - Should take care of the occasional but persistent "ERROR:
      install_driver(Pg) failed" errors.
    - DBD::Pg is not async signal safe, and gets in a permanently
      corrupted state if we unwind in the middle of a DB operation.
    - The only fix is to manually restart the service. So just exit
      cleanly and get the process automatically restarted.

--- src/Util/Watchdog.pm
+++ src/Util/Watchdog.pm
@@ -15,7 +15,15 @@ sub with_watchdog {
             last if $frame->subroutine() eq 'Util::Watchdog::with_watchdog';
         }
 
-        die "Request timed out\n";
+        print "Request timed out\n";
+        # Treat a watchdog timeout as a fatal error, rather than an
+        # catchable exception. DBD::Pg is not async signal safe, and
+        # gets in a permanently corrupted state if we unwind in the
+        # middle of a DB operation. That instance of the code will
+        # then not work again, producing user-visible errors until the
+        # service is manually restarted. So just quit now, and let
+        # the server process restart.
+        exit 1;
     };
 
     eval {
