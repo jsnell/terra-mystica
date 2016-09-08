877001f93d5b6d769d8a4298767ca7317a98b9aa
Juho Snellman <jsnell@iki.fi>
2016-09-08 13:14:49 +0200

    Discussion open for Loon lakes

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -35,13 +35,7 @@ fetchChangelog(function(news) {
 });
 </script>
 
-<div class="motd" style="display: none">
-A new community created map will be made available for playtesting soon.
-The general concept is to gather data for a specific map version for a
-couple of months, and revise it based on feedback and game results.
-
-<p>
-See the <a href="https://boardgamegeek.com/thread/1603489/">BGG thread</a>
-for the latest map revision, and take part in the discussion before the
-version to be used for the first playtest iteration is finalized.
+<div class="motd" style="display: block">
+The <a href="https://boardgamegeek.com/article/23645673#23645673">discussion
+is open</a> for a new revision of Loon Lakes.
 </div>
