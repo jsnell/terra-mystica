49cc136d7cb983bd732253fefabbad6d63e6c31f
Juho Snellman <jsnell@iki.fi>
2017-01-19 02:31:23 +0100

    season 17 open

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,9 +8,9 @@
 
 __DATA__
 
-<div class="motd" style="display: none">
-    Season 16 of the Terra Mystica tournament will start on December 1st.
-    Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1674178/tm-tournament-season-16">BGG thread</a>.
+<div class="motd" style="display: block">
+    Season 17 of the Terra Mystica tournament will start on February 1st.
+    Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1709993/tm-tour-season-17-signup-open">BGG thread</a>.
 </div>
 
 <h4>Your Active / Recently Finished Games</h4>
