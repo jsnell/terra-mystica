3bff582258e8ebbe6d323e75be584e7a14dd57b4
Juho Snellman <jsnell@iki.fi>
2017-08-05 06:37:34 +0200

    Season 20 signup over

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,7 +8,7 @@
 
 __DATA__
 
-<div class="motd" style="display: block">
+<div class="motd" style="display: none">
     Season 20 of the Terra Mystica tournament will start on August 1st.
     Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1813715/tm-tour-season-20-signup-open-games-aug-1">BGG thread</a>.
 </div>
