d8b1bde44cb1946feebe2e45a6e267b3e7a625cb
Juho Snellman <jsnell@iki.fi>
2017-11-19 17:52:56 +0100

    season 22

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,9 +8,9 @@
 
 __DATA__
 
-<div class="motd" style="display: none">
-    Season 21 of the Terra Mystica tournament will start on October 1st.
-    Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1849182/">BGG thread</a>.
+<div class="motd" style="display: block">
+    Season 22 of the Terra Mystica tournament will start on December 1st.
+    Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1886358/">BGG thread</a>.
 </div>
 
 <h4>Your Active / Recently Finished Games</h4>
