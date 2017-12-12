b315eb2c2d68f4f4d86c7ecfe49146808bfa2b3b
Juho Snellman <jsnell@iki.fi>
2017-12-12 20:58:57 +0100

    oops, should have taken s22 signup down earlier

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,7 +8,7 @@
 
 __DATA__
 
-<div class="motd" style="display: block">
+<div class="motd" style="display: none">
     Season 22 of the Terra Mystica tournament will start on December 1st.
     Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/1886358/">BGG thread</a>.
 </div>
