373a9edd9f8fa2116d1b6a0b658aee53228577e2
Juho Snellman <jsnell@iki.fi>
2019-06-17 01:08:29 +0200

    finalize Fjords

--- pages/content/index.pl
+++ pages/content/index.pl
@@ -8,7 +8,7 @@
 
 __DATA__
 
-<div class="motd" style="display: block">
+<div class="motd" style="display: none">
     Season 31 of the Terra Mystica tournament will start on June 1st.
     Sign ups are now open on the <a href="http://tmtour.org">tournament website</a>, with discussion on the <a href="https://boardgamegeek.com/thread/2206980/sign-season-31-tm-tour">BGG thread</a>.
 </div>
--- src/DB/SaveGame.pm
+++ src/DB/SaveGame.pm
@@ -103,7 +103,7 @@ sub create_game {
     }
 
     my $unranked = 1;
-    if ($map_variant =~ /^(|95a66999127893f5925a5f591d54f8bcb9a670e6|be8f6ebf549404d015547152d5f2a1906ae8dd90|fdb13a13cd48b7a3c3525f27e4628ff6905aa5b1|91645cdb135773c2a7a50e5ca9cb18af54c664c4)$/) {
+    if ($map_variant =~ /^(|95a66999127893f5925a5f591d54f8bcb9a670e6|be8f6ebf549404d015547152d5f2a1906ae8dd90|fdb13a13cd48b7a3c3525f27e4628ff6905aa5b1|91645cdb135773c2a7a50e5ca9cb18af54c664c4|2afadc63f4d81e850b7c16fb21a1dcd29658c392)$/) {
         $unranked = 0
     }
 
